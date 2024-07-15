ac381933d41706607682501083b87a92a57d3b11
Gulfia83 <168065597+Gulfia83@users.noreply.github.com>
2024-07-15 13:27:45 +0300

    optimize comments count evaluation.

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,7 @@ from django.db.models import Count
 
 
 def get_related_posts_count(tag):
-    return tag.posts_count
-
-
-def get_likes_count(post):
-    return post.likes_count
+    return tag.posts.count()
 
 
 def serialize_post(post):
@@ -30,7 +26,7 @@ def serialize_post_optimized(post):
         'title': post.title,
         'teaser_text': post.text[:200],
         'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
+        'comments_amount': post.comments_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
@@ -49,8 +45,14 @@ def serialize_tag(tag):
 def index(request):
     popular_posts = Post.objects.prefetch_related('author').annotate(likes_count=Count('likes')).order_by('-likes_count')
     most_popular_posts = popular_posts[:5]
+    most_popular_posts_ids = [post.id for post in most_popular_posts]
+    posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(comments_count=Count('comments'))
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+    for post in most_popular_posts:
+        post.comments_count = count_for_id[post.id]
 
-    fresh_posts = Post.objects.prefetch_related('author').order_by('published_at')
+    fresh_posts = Post.objects.prefetch_related('author').annotate(comments_count=Count('comments')).order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
 
     popular_tags = Tag.objects.annotate(posts_count=Count('posts')).order_by('-posts_count')
@@ -123,7 +125,7 @@ def tag_filter(request, tag_title):
     context = {
         'tag': tag.title,
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
-        'posts': [serialize_post_optimized(post) for post in related_posts],
+        'posts': [serialize_post(post) for post in related_posts],
         'most_popular_posts': [
             serialize_post(post) for post in most_popular_posts
         ],
